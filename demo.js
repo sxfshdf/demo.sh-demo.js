6c88cd9384d38b5f1c85adb085884a718fc635ae
sixiaofeng <sxf754872493@gmail.com>
2018-08-05 23:27:10 +0800

    修改demo.sh

--- demo.js
+++ demo.js
@@ -7,7 +7,7 @@ process.chdir("./" + dirName)
 fs.mkdirSync('css')
 fs.mkdirSync('js')
 
-fs.writeFileSync("./index.html","<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+fs.writeFileSync("./index.html","<!DOCTYPE>\n<title>Hello</title>/n<h1>Hi</h1>")
 fs.writeFileSync("./css/style.css","h1{color: red;}")
 fs.writeFileSync("./js/main.js","var string = 'Hello World' alert(string)")
 
